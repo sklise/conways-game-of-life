3baccbe7209755447aecf758bfd30e42d8f1602c
Steven Klise <steven@stevenklise.com>
2011-03-29 01:47:21 -0400

    Styling

--- public/js/conway.js
+++ public/js/conway.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
+    var name = 'steven';
+    var domain = 'stevenklise.com';
+    $('.email_sk').html('<a href="mailto:'+name+'@'+domain+'">'+name+'@'+domain+'</a>');
+	
+	// Initialize a few variables.
 	window.edges = true;
 	window.running = false;
 	
 	// Sketch Size
+	var cpheight = $('#controlpanel').height();
 	window.sketchWidth = $(window).width()-20;
-	window.sketchHeight = $(window).height()-10;
+	window.sketchHeight = $(window).height()-cpheight-15;
 	$('canvas').css('width',window.sketchWidth+'px').css('height',window.sketchHeight+'px');
-
+	$('#info').css('height',$(window).height());
 	// stretch control panel to full width.
 	$('#controlpanel').css('width',$(window).width()+'px');
 	
