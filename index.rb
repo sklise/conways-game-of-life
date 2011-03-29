b2849a0e766222b2a9951c87ba36a05369cc3003
Steven Klise <steven@stevenklise.com>
2011-03-28 22:49:25 -0400

    Basic database calls

--- index.rb
+++ index.rb
@@ -1,6 +1,32 @@
 require 'bundler'
 Bundler.require
+require 'patterns'
+
 
 get "/" do
   erb :main
+end
+
+get "/patterns/new" do
+  @categories = Category.all
+  erb :newpattern
+end
+
+post "/patterns/?" do
+  # params = { :name => GLIDER, :width => 3, :height => 3, :category => MOVER}
+end
+
+get "/categories" do
+  @allcategories = Category.all
+  erb :categories
+end
+
+get "/categories/new" do
+  erb :newcategory
+end
+
+post "/add_category" do
+  @category = Category.create(params)
+  @allcategories = Category.all
+  erb :categories
 end
