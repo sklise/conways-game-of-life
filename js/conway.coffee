e8f386a2409bb5dfaebe5dbfcbec3c41fd0579ca
Steve Klise <sk@skli.se>
2014-08-16 19:56:39 -0400

    Write Jasmine tests

--- js/conway.coffee
+++ js/conway.coffee
@@ -23,10 +23,8 @@ checkNeighbors = (n,world,width,height) ->
   worldSize = width * height - 1
 
   neighbors = getNeighborIndeces(n, width)
-  console.log neighbors
 
   while currentNeighbor < 8
-    console.log currentNeighbor, neighbors[currentNeighbor], world[neighbors[currentNeighbor]]
     # Short circuit if we are over
     break if neighborCount > 3
 
@@ -39,9 +37,7 @@ checkNeighbors = (n,world,width,height) ->
 
 # n is false so look for neighbors equal to 3
 nextFromDead = (n, world, width, height) ->
-  console.log "\nnext from dead"
   neighborCount = checkNeighbors(n,world,width,height)
-  console.log "=", neighborCount
   if neighborCount is 3 then true else false
 
 # n is true so look for for neighbor count of 2 or 3
@@ -55,44 +51,9 @@ w.nextGeneration = (n, world, width, height) ->
   else
     nextFromDead(n, world, width, height)
 
-
-# Tests
-world = [
-  no, no, no,
-  no, yes, no,
-  yes, yes, yes
-]
-
-console.log(true, nextGeneration(4, world, 3, 3))
-
-world2 = [
-  no, no, yes,
-  no, yes, yes,
-  yes, yes, yes
-]
-
-console.log(false, nextGeneration(4, world2, 3, 3))
-
-world3 = [
-  yes, no, no,
-  no, no, yes,
-  no, no, yes
-]
-
-console.log(true, nextGeneration(4, world3, 3, 3))
-
-world3 = [
-  yes, no, no,
-  no, no, no,
-  no, no, yes
-]
-
-console.log(false, nextGeneration(4, world3, 3, 3))
-
 conway = (s) ->
 
   s.setup = ->
-    console.log 'setup'
     s.createCanvas 900, 700
 
   # s.draw = ->
---
+++ test/conway_spec.js
@@ -0,0 +1,95 @@
+no = false
+yes = true
+
+describe("Conway", function () {
+  describe("nextFromDead()", function () {})
+  describe("nextFromLiving()",function () {})
+
+  describe("nextGeneration()", function () {
+    describe("cell is alive", function () {
+      it("should stay alive if neighbors are between 2 and 3", function () {
+        worlds = [
+          [
+            no, no, no,
+            no, yes, no,
+            yes, yes, yes
+          ],
+          [
+            yes, yes, yes,
+            no, yes, no,
+            no, no, no
+          ],
+          [
+            yes, yes, no,
+            yes, yes, no,
+            no, no, no
+          ],
+          // 2
+          [
+            yes, no, no,
+            no, yes, no,
+            no, no, yes
+          ],
+          [
+            yes, no, no,
+            yes, yes, no,
+            no, no, no
+          ]
+        ]
+
+        worlds.forEach(function (world ) {
+          expect(nextGeneration(4, world, 3, 3)).toBe(true)
+        })
+      })
+
+      it("should die if neighbors are less than 2 or greater than 3", function() {
+
+        world = [
+          no, no, yes,
+          no, yes, yes,
+          yes, yes, yes
+        ]
+
+        expect(nextGeneration(4, world, 3, 3)).toBe(false)
+      })
+    })
+
+    describe("cell is dead", function () {
+      it("should spawn a cell if neighbors are 3", function () {
+        worlds = [
+          [
+            yes, no, no,
+            no, no, yes,
+            no, no, yes
+          ],
+          [
+            yes, no, no,
+            no, no, no,
+            no, yes, yes
+          ]
+        ]
+
+        worlds.forEach(function (world) {
+          expect(nextGeneration(4,world,3,3)).toBe(true)
+        })
+      })
+
+      it("should not spawn a cell if neighbors are not 3", function () {
+        worlds = [
+          [
+            no, no, no,
+            no, no, yes,
+            no, no, yes
+          ],
+          [
+            yes, no, no,
+            yes, no, no,
+            yes, yes, yes
+          ]
+        ]
+        expect(nextGeneration(4, world, 3, 3)).toBe(false)
+      })
+
+    })
+  })
+})
